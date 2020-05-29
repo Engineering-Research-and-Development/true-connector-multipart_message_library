8e48226df62f2d5499800aaaf2e2cdf1cb5d65ed
Milan Karajovic <milan.karajovic@eng.it>
2020-05-29 13:33:36 +0200

    The library "Multipart Messsage Processor" for the project: Market 4.0

---
+++ src/main/java/it/eng/idsa/multipart/util/MultipartMessageKey.java
@@ -0,0 +1,27 @@
+package it.eng.idsa.multipart.util;
+
+/**
+ * 
+ * @author Milan Karajovic and Gabriele De Luca
+ *
+ */
+
+public enum MultipartMessageKey {
+	
+	EMPTY(""),
+	NAME_HEADER("header"),
+	NAME_PAYLOAD("payload"),
+	NAME_SIGNATURE("signature"),
+	CONTENT_TYPE("Content-Type"),
+	FORWARD_TO("Forward-To"),
+	CONTENT_DISPOSITION("Content-Disposition"),
+	CONTENT_LENGTH("Content-Length");
+ 
+    public final String label;
+ 
+    private MultipartMessageKey(String label) {
+        this.label = label;
+    }
+    
+}
+
